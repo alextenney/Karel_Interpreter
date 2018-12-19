10f9ae0cdf0fd06643ffc0587a44d3836a1885cf
alextenney <44792738+alextenney@users.noreply.github.com>
2018-12-19 00:27:03 -0700

    Update rplyparser.py

--- rplyparser.py
+++ rplyparser.py
@@ -3,63 +3,101 @@ from rply.token import BaseBox
 from rplylexer import lex
 import rply
 
+'''
+Content adapted from both the RPLY documentation and Josh Sharp's blog post "Using RPython and RPly to build a language interpreter, part 1".
+
+RPLY: https://media.readthedocs.org/pdf/rply/latest/rply.pdf and https://rply.readthedocs.io/en/latest/
+
+Josh Sharp: http://joshsharp.com.au/blog/rpython-rply-interpreter-1.html
+
+'''
+
 class Parser:
     def __init__(self):
+        self.indent_count = 0;
+        self.definition_list = []
+
         self.pg = ParserGenerator(
             # A list of all token names, accepted by the parser. 
-            ['STRING', 'INTEGER', 'IDENTIFIER', 'IF', 'ELSE', 'COLON', 'NOT', 'WHILE',  
-            'END', 'NEWLINE', 'FUNCTION', 'OPENPAREN', 'CLOSEPAREN', 'NOT', 'MODULE', 'IMPORT', 'BEGIN', 'MOVE',
+            ['INTEGER', 'IDENTIFIER', 'IF', 'ELSE', 'COLON', 'NOT', 'WHILE',  
+            'END', 'FUNCTION', 'OPENPAREN', 'CLOSEPAREN', 'NOT', 'IMPORT', 'BEGIN', 'MOVE',
             'LEFTTURN', 'PUTBEEPER', 'PICKBEEPER', 'FACENORTH', 'FACESOUTH', 'FACEWEST', 
-            'LEFTCLEAR', 'RIGHTCLEAR', 'PRESENT', 'INBAG', 'NOTCHECK', 'SPACE' 
+            'LEFTCLEAR', 'RIGHTCLEAR', 'FRONTCLEAR', 'PRESENT', 'INBAG', 'FOR',
+            'FACEEAST', 'INDENT', 'DEDENT'
             ],
             # A list of precedence rules with ascending precedence, to 
             # disambiguate ambiguous production rules. 
             precedence=[ 
-                ('left', ['FUNCTION',]), 
-                ('left', ['LET',]), 
-                ('left', ['=']), 
+                ('left', ['FUNCTION',]),  
                 ('left', ['[',']',',']), 
-                ('left', ['IF', 'COLON', 'ELSE', 'NEWLINE','WHILE',])
+                ('left', ['IF', 'COLON', 'ELSE', ' ','WHILE',])
             ] 
         )
 
-    def parse(self):
+    def setup(self):
         @self.pg.production('program : IMPORT lotsdef BEGIN parentheses lots END parentheses')
         def program(p):
-            print('This program works!"')
+            pass
+
         @self.pg.production('parentheses : OPENPAREN CLOSEPAREN')
         def parentheses(p):
-            print('I matched ()')
+            pass
+
         @self.pg.production('lots : ')
         def lots(p):
-            print('I matched something empty')
+            pass
+
         @self.pg.production('lots : statement lots')
         def bigone(p):
-            print('i found either a statment or nothing')
-        @self.pg.production('statement : MOVE parentheses')
-        @self.pg.production('statement : LEFTTURN parentheses')
+            pass
+
+        @self.pg.production('call : IDENTIFIER parentheses')
+        def call(p):
+            if p[0].value in self.definition_list:
+                pass
+            else:
+                raise ValueError('The definition '+ str(p[0].value)+ ' has been called before defined on line '+ str(p[0].lineno + 1))
+
+        @self.pg.production('statement : MOVE parentheses  ')
+        @self.pg.production('statement : LEFTTURN parentheses  ')
         @self.pg.production('statement : PUTBEEPER parentheses')
         @self.pg.production('statement : PICKBEEPER parentheses')
+        @self.pg.production('statement : call')
+        @self.pg.production('statement : IF condition  COLON INDENT lots DEDENT')
+        #it for some reason does not recognize this kind of statement
+        @self.pg.production('statement : IF condition  COLON INDENT lots DEDENT ELSE  COLON INDENT lots DEDENT')
+        @self.pg.production('statement : WHILE condition  COLON INDENT lots DEDENT')
+        #the for loop is broken because the identifier is broken
+        @self.pg.production('statement : FOR OPENPAREN INTEGER CLOSEPAREN  COLON INDENT lots DEDENT')
         def statement(p):
-            print('found a statment')
+            pass
+
+        @self.pg.production('condition : FACENORTH parentheses')
+        @self.pg.production('condition : FACESOUTH parentheses')
+        @self.pg.production('condition : FACEEAST parentheses')
+        @self.pg.production('condition : FACEWEST parentheses')
+        @self.pg.production('condition : FRONTCLEAR parentheses')
+        @self.pg.production('condition : LEFTCLEAR parentheses')
+        @self.pg.production('condition : RIGHTCLEAR parentheses')
+        @self.pg.production('condition : PRESENT parentheses')
+        @self.pg.production('condition : INBAG parentheses')
+        @self.pg.production('condition : NOT condition')
+        def condition(p):
+            pass
+
         @self.pg.production('lotsdef : definition lotsdef')
         @self.pg.production('lotsdef : ')
-        @self.pg.production('definition : FUNCTION SPACE IDENTIFIER parentheses COLON indent lots')
+        def lotsdef(p):
+            pass
+        @self.pg.production('definition : FUNCTION IDENTIFIER parentheses COLON INDENT lots DEDENT')
         def definition(p):
-            print('found a definition')
-        @self.pg.production('indent : SPACE SPACE SPACE SPACE')
+            self.definition_list.append(p[1].value)
+            pass
+        
 
         @self.pg.error
         def error_handler(token):
-            raise ValueError("Ran into a %s where it wasn't expected" % token.gettokentype())
-        '''
-        def error_handler(token):
-            raise ValueError("Ran into a %s where it wasn't expected" % token.gettokentype())
+            raise ValueError("Ran into a %s where it wasn't expected" % token.gettokentype()+ " at line "+ str(token.lineno + 1))
 
-    parser = pg.build()
-    '''
-    
-    def get_parser(self):
-        return self.pg.build()
-
-#SO WE ARE IGNORING NOTHING, WE HAVE A TOKEN FOR SPACE AND FOR NEW LINES. iF WE CAN DO A: IF # SPACE %4 == 0: SPACE = INDENT// IGNORE ALL OTHERS
+    def parse(self, tokens):
+        return self.pg.build().parse(tokens)
